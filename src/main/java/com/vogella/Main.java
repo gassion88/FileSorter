c270b2547a0d9b0dbd036d1c4c92382b84f21dce
Soslan Gassiev <soslangassity04@gmail.com>
2023-11-20 19:09:38 +0300

    refactor :

--- src/main/java/com/vogella/Main.java
+++ src/main/java/com/vogella/Main.java
@@ -9,26 +9,26 @@ public class Main {
     private static final FilePartsUploader filePartsUploader = new FilePartsUploader();
     private static final FilePartsCombiner filePartsCombiner = new FilePartsCombiner();
     private static final PartsToFileWriter partsToFileWriter = new PartsToFileWriter();
-    private static List<String> PARTS = new ArrayList<>();
+    private static List<String> fileParts = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         externalSort("c:/temp/first.txt", "c:/temp/second.txt", 1000000);
     }
 
     public static void externalSort(String inputFile, String outputFile, long memoryLimit) throws IOException {
-        long PART_SIZE = memoryLimit / 2;
+        long part = memoryLimit / 2;
 
-        PARTS = filePartsUploader.upload(inputFile, PART_SIZE);
+        fileParts = filePartsUploader.upload(inputFile, part);
 
-        PriorityQueue<BufferedReader> sortedParts = filePartsCombiner.combine(PARTS);
+        PriorityQueue<BufferedReader> sortedParts = filePartsCombiner.combine(fileParts);
         
         partsToFileWriter.write(sortedParts, outputFile);
 
-        removeTemproaryFiles(PARTS);
+        removeTemproaryFiles(fileParts);
     }
 
     private static void removeTemproaryFiles(List<String> PARTS) {
-        for (String part : Main.PARTS) {
+        for (String part : Main.fileParts) {
             new File(part).delete();
         }
     }
