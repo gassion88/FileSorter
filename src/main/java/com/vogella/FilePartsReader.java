5a6b4017dc08bc66fb7cfafdebb02b2211c690ff
Soslan Gassiev <soslangassity04@gmail.com>
2023-11-20 23:20:02 +0300

    refactor :

--- src/main/java/com/vogella/FilePartsReader.java
+++ src/main/java/com/vogella/FilePartsReader.java
@@ -6,11 +6,9 @@ import java.util.Comparator;
 import java.util.List;
 
 public class FilePartsReader {
-    long partSize = 0;
-    List<String> parts = new ArrayList<>();
-
     public List<String> upload(String inputFile, long memoryLimit) {
-        partSize = memoryLimit;
+        List<String> filePartsNames = new ArrayList<>();
+        long partSize = memoryLimit / 2;
 
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
             String line;
@@ -19,40 +17,50 @@ public class FilePartsReader {
             while ((line = br.readLine()) != null) {
                 lines.add(line);
                 if (lines.stream().mapToLong(String::length).sum() > partSize) {
-                    generatePartFile(inputFile, lines);
+                    String filePartName = createFilePart(inputFile, lines, filePartsNames.size());
+                    writeToFilePart(filePartName, lines);
+                    filePartsNames.add(filePartName);
+
                     lines.clear();
                 }
             }
 
             if (!lines.isEmpty()) {
-                generatePartFile(inputFile, lines);
+                String filePartName = createFilePart(inputFile, lines, filePartsNames.size());
+                writeToFilePart(filePartName, lines);
+                filePartsNames.add(filePartName);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return parts;
+        return filePartsNames;
     }
 
-    private void generatePartFile(String inputFile, List<String> lines) throws IOException {
-        partLinesSort(lines);
-        String partFile = generatePartFileName(inputFile);
+    private String createFilePart(String inputFile, List<String> lines, int size) {
+        String filePartName = generateFilePartName(inputFile, size);
+        filePartLinesSort(lines);
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(partFile))) {
+        return filePartName;
+    }
+
+    private void writeToFilePart(String filePartName, List<String> lines) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePartName))) {
             for (String line : lines) {
                 bw.write(line);
                 bw.newLine();
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        parts.add(partFile);
     }
 
-    private static void partLinesSort(List<String> lines) {
+    private static void filePartLinesSort(List<String> lines) {
         lines.sort(Comparator.naturalOrder());
     }
 
-    private String generatePartFileName(String inputFile) {
+    private String generateFilePartName(String inputFile, int size) {
         String fileName = inputFile.split("\\.")[0];
-        return fileName + "_chunk" + parts.size() + ".txt";
+        return fileName + "_chunk" + size + ".txt";
     }
 }
