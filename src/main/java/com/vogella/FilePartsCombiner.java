c89e4d337fc374224bac53e6306e3094e8c93e2f
Soslan Gassiev <soslangassity04@gmail.com>
2023-11-23 18:28:44 +0300

    feat :

--- src/main/java/com/vogella/FilePartsCombiner.java
+++ src/main/java/com/vogella/FilePartsCombiner.java
@@ -1,25 +1,27 @@
 package com.vogella;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Comparator;
+import java.io.*;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class FilePartsCombiner {
-    public PriorityQueue<String> combine(List<String> parts) throws IOException {
-        PriorityQueue<String> heap = new PriorityQueue<>();
-
-        for (String part : parts) {
-            try(BufferedReader br = new BufferedReader(new FileReader(part))) {
-                String line;
-                while ((line = br.readLine()) != null){
-                    heap.add(line);
+    public void combineAndWrite(List<String> parts, String outputFile, int chunkSize) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String part : parts) {
+                try(BufferedReader br = new BufferedReader(new FileReader(part))) {
+                    String line;
+                    int count = 0;
+                    while ((line = br.readLine()) != null){
+                        writer.write(line);
+                        writer.newLine();
+                        count++;
+                        if (count == chunkSize) {
+                            writer.flush();
+                            count = 0;
+                        }
+                    }
                 }
             }
         }
-
-        return heap;
     }
 }
